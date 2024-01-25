a4043f677b7425cc1d622668a365f5834ea7e7fa
Richard Gingrich <richardgingrich8@gmail.com>
2024-01-25 01:03:59 -0700

    small progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 // Global Vars and Constants
 const URL = "https://rgingrich8.github.io/";
 const URL_ALT = "https://rgingrich8.github.io";
+// let globalX = 0;
+// let globalY = 0;
 
 // Run
 main();
@@ -16,4 +18,16 @@ function mouseLogic(event) {
     let mouse = document.getElementById("mouse_basic");
     mouse.style.left = event.pageX + "px";
     mouse.style.top = event.pageY + "px";
+    // globalX = event.pageX;
+    // globalY = event.pageY;
 }
+
+// Mouse scroll logic, unfinished
+// function mouseScroll() {
+//     // console.log(document.documentElement.scrollTop);
+//     let mouse = document.getElementById("mouse_basic");
+//     let calc = document.documentElement.scrollTop + globalY;
+//     console.log("Calc: " + calc + " GlobalY: " + globalY);
+//     mouse.style.left = globalX + "px";
+//     mouse.style.top = calc + "px";
+// }
