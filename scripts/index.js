5281b3c3c15e157c2722d1da06bbf50a5c8fa796
RGingrich8 <richardgingrich8@gmail.com>
2025-04-28 22:20:48 +0200

    added: message for restructure

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 // Control Flow
 function main() {
-    console.log("index.js loaded successfully.")
+    console.log("index.js loaded successfully.");
+    alert("This website is being restructured. It will be completed by May 10, 2025.");
 }
 
 // Run
