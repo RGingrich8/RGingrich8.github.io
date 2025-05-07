1da032125657cd75f1ed61094620179329bef749
RGingrich8 <richardgingrich8@gmail.com>
2025-05-07 20:01:18 +0200

    added: small changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 // Control Flow
 function main() {
     console.log("index.js loaded successfully.");
-    alert("This website is being restructured. It will be completed by May 10, 2025.");
+    
+    // alert("This website is being restructured. It will be completed by May 10, 2025.");
 }
 
 // Run
