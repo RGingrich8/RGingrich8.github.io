2af2f364291bb6ee47f210dc3a7b29c7140206a3
RGingrich8 <richardgingrich8@gmail.com>
2025-05-11 01:00:05 +0200

    added: restructuring of website, single-page view with dropdowns

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 function main() {
     console.log("index.js loaded successfully.");
     
-    // alert("This website is being restructured. It will be completed by May 10, 2025.");
+    alert("This website is being restructured. It will be completed by May 10, 2025.");
 }
 
 // Run
